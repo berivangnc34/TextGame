be6ab374b2a309c90bd2733bd64b35a55f3d5573
BerivanGenc <berivangencb@gmail.com>
2021-07-26 22:54:29 +0300

    Metin bazlı geliştirilen 2d oyun projesi

---
+++ Assets/scripts/durum.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+[CreateAssetMenu(menuName = "durum")] //create menusunde çıkar.
+public class durum : ScriptableObject
+{
+    [TextArea(12, 10)][SerializeField] string hikayemetni; //serial yerine public yazabilirsin
+    //12 satır olsun,10dan sonra scroolbar çıksın
+
+    public durum[] sonrakidurumlar; //durum tipinde dizi
+
+    public string HikayeMetniniAl()
+    {
+        return hikayemetni;
+    }
+    public durum[] SonrakiDurumlariAl()
+    {
+        return sonrakidurumlar;
+    }
+   
+}
---
+++ Assets/scripts/oyunakisi.cs
@@ -0,0 +1,64 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class oyunakisi : MonoBehaviour
+{
+
+    [SerializeField] Text hikayeyazisi;
+    public durum baslangıcdurum;
+    durum gunceldurum;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        gunceldurum = baslangıcdurum;
+        //hikayeyazisi.text = baslangıcdurum.HikayeMetniniAl();
+        hikayeyazisi.text = gunceldurum.HikayeMetniniAl();
+
+
+
+
+        //hikayeyazısı textini oluşturdugun hikayeyazisina atıyorsun. o yüzden hikaye yazısı içindeki yazı değişiyor.
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        durum[] sonrakidurumlar = gunceldurum.SonrakiDurumlariAl(); //digee scripten çektik
+          
+       /* for(int i=0; i < sonrakidurumlar.Length; i++)
+        {
+            if (Input.GetKeyDown(KeyCode.Alpha1 + i))
+            {
+
+                gunceldurum = sonrakidurumlar[i];
+            }
+            
+        } */
+        
+        
+        
+        
+        //var sonrakidurumlar = gunceldurum.SonrakiDurumlariAl();
+
+         if (Input.GetKeyDown(KeyCode.Alpha1))
+         {
+             gunceldurum = sonrakidurumlar[0];
+         }
+         else if (Input.GetKeyDown(KeyCode.Alpha2))
+         {
+             gunceldurum = sonrakidurumlar[1];
+         }
+         else if (Input.GetKeyDown(KeyCode.Alpha3))
+         {
+             gunceldurum = sonrakidurumlar[2];
+         }
+         hikayeyazisi.text = gunceldurum.HikayeMetniniAl();
+
+
+     }
+     
+    }
+
